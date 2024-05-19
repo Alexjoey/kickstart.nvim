4414e3c6f3770a97e9e02d869da2a6aa640e0d18
Alex Mylle <alexm@live.be>
2024-05-19 17:29:44 +0200

    fixed fucked up tab

--- init.lua
+++ init.lua
@@ -862,7 +862,7 @@ require('lazy').setup({
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 vim.bo.softtabstop = 4
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
